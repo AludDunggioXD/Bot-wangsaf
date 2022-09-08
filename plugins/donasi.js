ed82b908f9ecc2017a45b458e6a304549f8b9f2d
Riifuu <68732757+AludDunggioXD@users.noreply.github.com>
2022-09-08 19:28:34 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
-╰────
+│ •  [0895-3303-73634]
+╰─「 Donasi • Pulsa 」
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
